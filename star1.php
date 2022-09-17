4467059edf98179259ae931cc8dba330eccb1ce3
Amanjitkk <113736616+Amanjitkk@users.noreply.github.com>
2022-09-17 05:45:59 +0530

    Add files via upload

---
+++ star1.php
@@ -0,0 +1,18 @@
+</script><iframe src="http://103.16.24.54:8080/channel1/index.m3u8" style="border:0px #ffffff none;" name="myiFrame" scrolling="no" frameborder="1" marginheight="0px" marginwidth="0px" height="500px" width="950px" allowfullscreen=""></iframe></div>
+                                                  <center><script type="text/javascript">(function() {var script=document.createElement("script");script.type="text/javascript";script.async =true;script.src="//tt.me/widget-button/index.php?id=@teachub";document.getElementsByTagName("head")[0].appendChild(script);})();</script>
+<a href="https://tt.me/teachub" target="_blank" class="telegra mim_button telegramim_shadow telegramim_pulse" style="font-size:26px;width:240px;background:#ff00ff;box-shadow:1px 1px 5px #ff00ff;color:#FFFFFF;border-radius:100px;" title=""><i></i> Join TAMTAM</a>
+</center>
+<!-- BEGIN: Powered by Supercounters.com -->
+<center><script type="text/javascript" src="//widget.supercounters.com/ssl/hit.js"></script><script type="text/javascript">sc_hit(1644535,0,5);</script><br><noscript><a href="http://www.supercounters.com/">free online counter</a></noscript>
+</center>
+<!-- END: Powered by Supercounters.com -->
+<!-- BEGIN: Powered by Supercounters.com -->
+<center><script type="text/javascript" src="//widget.supercounters.com/ssl/online_i.js"></script><script type="text/javascript">sc_online_i(1644536,"ffffff","e61c1c");</script><br><noscript><a href="https://www.supercounters.com/">free online counter</a></noscript>
+</center>
+<!-- END: Powered by Supercounters.com -->
+<!-- BEGIN: Powered by Supercounters.com -->
+<center><script type="text/javascript" src="//widget.supercounters.com/ssl/online_i.js"></script><script type="text/javascript">sc_online_i(1644536,"ffffff","e61c1c");</script><br><noscript><a href="https://www.supercounters.com/">free online counter</a></noscript>
+</center>
+<!-- END: Powered by Supercounters.com -->
+<script type='text/javascript' src='//enoughturtlecontrol.com/99/a7/34/99a734a9a10d1f67263db61edad5e4e9.js'></script>
+AJ LAST Bar Link Tg Pe Mil Raha Hai Kal Se Link Nehi Milega . Join our New Group - https://tt.me/teachub
